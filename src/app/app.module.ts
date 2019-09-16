bbe5747c845549b7612e9634be20f189faef6b62
Tim Chosen <timchosen@gmail.com>
2019-09-16 20:10:09 +0100

    innitial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -9,12 +10,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Rave, RavePayment, Misc } from 'rave-ionic4';
+// import { InAppBrowser, InAppBrowserEvent, InAppBrowserObject } from '@ionic-native/
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [HttpClientModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
+    InAppBrowser,
+    Rave, 
+    RavePayment, 
+    Misc,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { InAppBrowser, InAppBrowserEvent, InAppBrowserObject } from '@ionic-native/in-app-browser/ngx';
+import { Rave, RavePayment, Misc } from 'rave-ionic4';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+ amount:number = 3700.00;
+
+  constructor(
+  private rave: Rave, 
+  private ravePayment: RavePayment, 
+  private misc: Misc,
+  private iab: InAppBrowser
+    ) { }
+
+
+
+ ravePay() {
+      this.rave.init(true, "FLWPUBK-77350cbb62c4eb4aa8c4b78b6ff28df1-X")
+      .then(_ => {
+        var paymentObject = this.ravePayment.create({
+          customer_email: "user@example.com",
+          amount: this.amount,
+          customer_phone: "234099940409",
+          currency: "NGN",
+          txref: "rave-123456",
+          meta: [{
+              metaname: "flightID",
+              metavalue: "AP1234"
+          }]
+      })
+        this.rave.preRender(paymentObject)
+          .then(secure_link => {
+            secure_link = secure_link +" ";
+            const browser: InAppBrowserObject = this.rave.render(secure_link, this.iab);
+            browser.on("loadstop")
+                .subscribe((event: InAppBrowserEvent) => {
+                  if(event.url.indexOf('http://localhost:8100/home') != -1) {
+                    if(this.rave.paymentStatus('url') == 'failed') {
+                      console.log("Message", "Oops! Transaction failed");
+                    }else {
+                      console.log("Message", "Transaction Succesful");
+
+                    }
+                    browser.close()
+                  }
+                })
+          }).catch(error => {
+            // Error or invalid paymentObject passed in
+            console.log ("error", error);
+          })
+      })
+
+    }
 
 }
